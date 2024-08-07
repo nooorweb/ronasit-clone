61f0fbe6a484c4a14c003367e3664316b5d5cd16
nooorweb <noorulaminweb@gmail.com>
2024-08-07 18:47:58 +0500

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import './App.css'
 import Home from './Home'
 import Header from './Header'
@@ -6,9 +6,11 @@ import Calculater from './Calculater'
 import Home2 from './Home2'
 import Footer from './Footer'
 
+
 function App() {
   return (
 <div>
+
 <Header/> 
 <Home/>
 <Calculater/>
